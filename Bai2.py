7487715ee43212354aa70b2ae1bc2118395e231d
nhatminhhangbot <nhatminh.hangbot@gmail.com>
2026-05-19 15:47:11 +0700

    initial commit

---
+++ Bai2.py
@@ -0,0 +1,19 @@
+print("--- HỆ THỐNG GỬI EMAIL THƯỞNG TẾT ---")
+
+# Vòng lặp chạy đúng 3 lần cho 3 nhân viên
+for employee_number in range(1, 4):
+    print("--- Đang xử lý nhân viên số", employee_number, "---")
+    
+    # Yêu cầu kế toán nhập dữ liệu
+    working_days = int(input("Nhập số ngày công trong tháng: "))
+
+    # Kiểm tra điều kiện
+    if working_days == 0:
+        print("CẢNH BÁO: Nhân viên nghỉ cả tháng. Không xét duyệt thưởng.")
+        continue
+
+    bonus_amount = working_days * 200000
+    print("-> Đã gửi Email: Chúc mừng nhận được", bonus_amount, "VNĐ tiền thưởng!")
+    print("------------------------------------------\n")
+
+print("Đã hoàn tất quá trình duyệt thưởng cho 3 nhân viên!")
